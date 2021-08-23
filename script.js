0a74886f5a927fa7e91d0df39499103572f0d95d
Chris Soncrant <christopher.soncrant@gmail.com>
2021-08-23 12:09:25 -0700

    added the clear button and the grid value setter

--- script.js
+++ script.js
@@ -1,40 +1,88 @@
-const main = document.querySelector(".main")
+const main = document.querySelector(".main");
+
+const button = document.querySelector("button");
+
+let gridValue;
+
+let totalSquares;
 
 /* main.style.cssText = "height: 300px; width: 500px; border: 2px solid black;"
  */
 
-function colorChange () {
-    main.addEventListener('mouseenter', () => {
-        main.style.backgroundColor = "teal";
-        main.style.transition = "all 0s";
-    })
-
-    main.addEventListener('mouseleave', () => {
-        main.style.backgroundColor = "orange";
-        main.style.transition = "all 5s";
-    })
-};
-
 for (let i = 256; i > 0; i--) {
     const square = document.createElement("div");
-    square.classList.add("squares", "bd");
+    square.classList.add("squares");
     main.appendChild(square);
 }
 
-const squares = document.querySelectorAll('.squares');
+let squares = document.querySelectorAll('.squares');
+
+button.addEventListener("click", () => {
+    squares.forEach(a => {
+        a.style.backgroundColor = "white"
+    })
+
+    gridValue = prompt("How big of a grid?") * 1;
+
+    if (gridValue <= 100) {
+        totalSquares = gridValue * gridValue;
+    
+        main.style.gridTemplateColumns = `repeat(${gridValue}, 1fr)`;
+
+        main.style.gridTemplateRows = `repeat(${gridValue}, 1fr)`;
+
+        for (let i = totalSquares; i > 0; i--) {
+            const square = document.createElement("div");
+            square.classList.add("squares");
+            main.appendChild(square);
+        }
+    
+        squares = document.querySelectorAll('.squares');
+
+        squares.forEach(a => {
+            a.addEventListener('mouseenter', () => {
+                a.style.backgroundColor = "aqua"; //bc;
+                a.style.transition = "all 0s";
+            })
+        
+            a.addEventListener('click', () => {
+                a.style.backgroundColor = "white";
+            })
+        
+             /* a.addEventListener('mouseleave', () => {
+                a.style.backgroundColor = "white";
+                a.style.transition = "all 5s";
+            }) */
+        })
+    } else {alert("enter a number less than 100! Press button again")}
+    
+})
 
 squares.forEach(a => {
     a.addEventListener('mouseenter', () => {
-        a.style.backgroundColor = "aqua";
+        a.style.backgroundColor = "aqua"; //bc;
         a.style.transition = "all 0s";
     })
 
-    a.addEventListener('mouseleave', () => {
+    a.addEventListener('click', () => {
+        a.style.backgroundColor = "white";
+    })
+
+     /* a.addEventListener('mouseleave', () => {
         a.style.backgroundColor = "white";
         a.style.transition = "all 5s";
-    })
+    }) */
 })
 
+/* let bc;
+
+setInterval(() => {
+    let x = Math.floor(Math.random() * 257);
+    let y = Math.floor(Math.random() * 257);
+    let z = Math.floor(Math.random() * 257);
+    return bc = `rgb(${x} ${y} ${z})`;
+}, 2000)
+ */
 
 
 /* function enableBackgroundColor() {
@@ -43,9 +91,7 @@ squares.forEach(a => {
     let interval = setInterval(() => {
         if (z === 256) {return clearInterval(interval)};
         if (main.children[i].style.backgroundColor === '') {
-            let x = Math.floor(Math.random() * 257);
-            let y = Math.floor(Math.random() * 257);
-            let z = Math.floor(Math.random() * 257);
+            
             main.children[i].style.backgroundColor = `rgb(${x}, ${y}, ${z})`};
             i++;
             z++;
